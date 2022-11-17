831c3e39d23b5a26048b6ef1054fa9f829c23b79
Nadia Potteiger <nyadiia@pm.me>
2022-11-17 13:17:08 -0600

    Reverted back to a simple ping-pong bot

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,32 @@
 use std::env;
 
-use serenity::Client;
-use slashy::{
-    framework::{
-        Framework,
-        CommandContext,
-    },
-    commands::CommandResult,
-    settings::Settings,
-    command,
-    subcommand
-};
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
 
-command!{
-    ping,
-    "ping pong",
-    pong,
-    [
-        optional String text | "text to echo"
-    ]
-}
+#[group]
+#[commands(ping)]
+struct General;
 
-#[subcommand]
-async fn pong(ctx: &CommandContext) -> CommandResult {
-    ctx.send_str(&format!("pong {}", ctx.get("text").unwrap_or("")))
-}
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-    let token = std::env::var("DISCORD_TOKEN").expect("token");
-    let app_id = std::env::var("APPLICATION_ID").expect("app_id").parse().expect("app_id parse");
-
-    // Create the slashy framework
-    let settings = Settings {
-        prefixes: vec!["!"],
-        auto_register: true,
-        auto_delete: true,
-        slash_command_guilds: vec![]
-    };
-    let framework = Framework::new(settings, app_id, token)
-        .command(PING_COMMAND);
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
 
     // Login with a bot token from the environment
-    let mut client = Client::builder(token)
-        .event_handler(framework)
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
         .await
         .expect("Error creating client");
 
@@ -51,4 +34,11 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
 }
